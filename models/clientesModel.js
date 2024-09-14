55432afa7f4c4d56d5ea6d80adb1ab680c55a21d
Pietro Urbina <urbinapala2005@gmail.com>
2024-09-14 15:06:50 -0500

    se actualizo los campos del model

--- models/clientesModel.js
+++ models/clientesModel.js
@@ -2,12 +2,11 @@ import db from "../database/db.js";
 import { DataTypes } from "sequelize";
 
 const clientesModel = db.define('cliente', {
+    dni: { type: DataTypes.STRING },
     nombre: { type: DataTypes.STRING },
     apellido: { type: DataTypes.STRING },
-    dni: { type: DataTypes.STRING },
     email: { type: DataTypes.STRING },
     telefono: { type: DataTypes.STRING },
-    direccion: { type: DataTypes.STRING },
     fecha_nacimiento: { type: DataTypes.DATE }
     
 }, {
