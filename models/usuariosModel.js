c8abde5ba1594c10346e46356302bffb8479e95d
Pietro Urbina <urbinapala2005@gmail.com>
2024-10-10 00:08:38 -0500

    .

--- models/usuariosModel.js
+++ models/usuariosModel.js
@@ -1,1 +1,22 @@
-m2       
+import db from "../database/db.js";
+import { DataTypes } from "sequelize";
+import empleadosModel from "./empleadosModel.js"
+
+const productosModel = db.define('productos', {
+    nombre_usuario: { type: DataTypes.STRING },
+    contrasena: { type: DataTypes.STRING },
+    rol: { 
+        type: DataTypes.ENUM('Administrador',  'Cajero', 'Mozo'),
+        allowNull: false  // Asegura que siempre se registre un valor
+    },
+    id_empleado: { type: DataTypes.INTEGER },    
+}, {
+    timestamps: false  // Opcional: deshabilitar createdAt y updatedAt
+});
+
+// Establecer relación con categorías
+usuariosModel.belongsTo(empleadosModel, { foreignKey: 'id_empleado' });
+
+
+export default productosModel;
+
